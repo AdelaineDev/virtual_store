b44c2870ebb44fe87635045b6404263559d9f1fe
AdelaineDev <adelainesant18@gmail.com>
2023-02-07 10:34:39 -0300

    feat: implementa models

---
+++ src/main/java/com/devhub/catalog/model/Produto.java
@@ -0,0 +1,41 @@
+package com.devhub.catalog.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_produto")
+@Entity
+public class Produto  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(columnDefinition = "Text")
+    private String description ;
+    private Double price;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant date;
+
+
+
+    @ManyToMany
+    @JoinTable(name = "tb_produto_categoria",
+            joinColumns = @JoinColumn(name = "produto_id"),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+    Set<Categoria> categories = new HashSet<>();
+
+
+}
