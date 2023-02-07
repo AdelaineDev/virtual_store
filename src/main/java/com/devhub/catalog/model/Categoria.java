76aaad84d9f0c2cd682eb5f2fb963913688f2b2a
Adelaine Santos <90655125+AdelaineDev@users.noreply.github.com>
2023-02-07 10:55:20 -0300

    Adiciona as entidades  (#1)
    * feat: implementa models
    
    * feat: configura database h2
    
    * style: remove linha em branco

---
+++ src/main/java/com/devhub/catalog/model/Categoria.java
@@ -0,0 +1,29 @@
+package com.devhub.catalog.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_categoria")
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updateAt;
+
+}
